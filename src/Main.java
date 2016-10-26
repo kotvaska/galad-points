3c7157eb9291fa6d1356858f0915035083e30f4e
Anastasia Zolotykh <zolotykh24@gmail.com>
2016-10-26 16:24:39 +0500

    Added getting coordinates for 4 points

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,49 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        double x1 = 2.0d, x2 = 20.0d, y1 = -1.0d, y2 = 5.0d;
-        Point point1 = new Point(x1, y1);
-        Point point2 = new Point(x2, y2);
-        List<Point> points = new ArrayList<>();
-        points.add(point1);
-        points.add(point2);
-        Vector vector = new Vector(points);
+        double x1 = 0.0d, x2 = 5.0d, y1 = 0.0d, y2 = 5.0d;
+        Point A = new Point(x1, y1);
+        Point B = new Point(x2, y2);
+        Vector vector = printLength(A, B);
 
-        double l = 5.0d;
-        double lOxAngle = 90.0d - Math.toDegrees(getAngle(x2 - x1, vector.getLength()));
+        double baseAngle = getAngle(x2 - x1, vector.getLength());
+        double l = 6.0d;
+        double angle = 90.0d - Math.toDegrees(baseAngle);
 
-        // the same point as vector
-        Point pointL1 = new Point(x1, y1);
-        x2 = Math.cos(lOxAngle) * l;
-        y2 = Math.sin(lOxAngle) * l;
-        Point pointL2 = new Point(x2, y2);
-        points = new ArrayList<>();
-        points.add(pointL1);
-        points.add(pointL2);
-        Vector lVector = new Vector(points);
+        x2 = A.getX() + Math.cos(angle / 180 * Math.PI) * l;
+        y2 = A.getY() - Math.sin(angle / 180 * Math.PI) * l;
+        Point A1 = new Point(x2, y2);
+        printLength(A, A1);
 
-        System.out.println(lVector.getLength());
+        x2 = A.getX() - Math.cos(angle / 180 * Math.PI) * l;
+        y2 = A.getY() + Math.sin(angle / 180 * Math.PI) * l;
+        Point A2 = new Point(x2, y2);
+        printLength(A, A2);
+
+        x2 = A1.getX() + Math.cos(baseAngle / 180 * Math.PI) * vector.getLength();
+        y2 = A1.getY() + Math.sin(baseAngle / 180 * Math.PI) * vector.getLength();
+        Point B1 = new Point(x2, y2);
+        printLength(A1, B1);
+
+        x2 = A2.getX() + Math.cos(baseAngle / 180 * Math.PI) * vector.getLength();
+        y2 = A2.getY() + Math.sin(baseAngle / 180 * Math.PI) * vector.getLength();
+        Point B2 = new Point(x2, y2);
+        printLength(A2, B2);
     }
 
     private static double getAngle(double len1, double len2) {
         return Math.acos(Math.toRadians(len1 / len2));
+    }
+
+    private static Vector printLength(Point ... p) {
+        List<Point> points = new ArrayList<>();
+        points.add(p[0]);
+        points.add(p[1]);
+        Vector vector = new Vector(points);
+
+        System.out.println(vector.getLength());
+
+        return vector;
     }
 
 }
